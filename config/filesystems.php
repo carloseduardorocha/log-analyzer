88ecbc59ab29f5483f232b0cce7b0c3e693156b0
Carlos Rocha <carlos.rocha548@hotmail.com>
2024-12-18 17:14:52 -0300

    feat(filesystem): updated public root config

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,7 +39,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => storage_path('app/logs-directory'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
