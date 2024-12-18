4227083745f51acba3c71dfbf49e4c2ced4cca38
Carlos Rocha <carlos.rocha548@hotmail.com>
2024-12-18 17:13:27 -0300

    doc(interface): add processLogData method documentation

--- app/Contracts/LogServiceContract.php
+++ app/Contracts/LogServiceContract.php
@@ -4,4 +4,14 @@ namespace App\Contracts;
 
 interface LogServiceContract
 {
+    /**
+     * Process the log data and save the associated consumer, service, and log.
+     *
+     * This method validates the incoming data properties, retrieves or creates the
+     * consumer and service entries, and then logs the data into the database within a transaction.
+     *
+     * @param array $data
+     * @return bool
+     */
+    public function processLogData(array $data): bool;
 }
