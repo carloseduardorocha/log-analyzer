f041be6eba8d36b2ab421b17bfd8fe433e34a1c7
Carlos Rocha <carlos.rocha548@hotmail.com>
2024-12-17 23:27:11 -0300

    feat(model): created Service model

---
+++ app/Models/Service.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+/**
+ * Class Service
+ *
+ * @property int $id
+ * @property string $uuid
+ * @property string|null $name
+ * @property string $created_at
+ * @property string|null $updated_at
+ * @property string|null $deleted_at
+ *
+ * @package App\Models
+ */
+class Service extends Model
+{
+    use HasFactory;
+
+    public const ID         = 'id';
+    public const UUID       = 'uuid';
+    public const NAME       = 'name';
+    public const CREATED_AT = 'created_at';
+    public const UPDATED_AT = 'updated_at';
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'services';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int,string>
+     */
+    protected $fillable = [
+        self::UUID,
+        self::NAME
+    ];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array<string,string>
+     */
+    protected $casts = [
+        self::CREATED_AT => 'datetime',
+        self::UPDATED_AT => 'datetime',
+    ];
+
+    /**
+     * Get the logs for the service.
+     *
+     * @return HasMany<Log>
+     */
+    public function logs(): HasMany
+    {
+        return $this->hasMany(Log::class, Log::SERVICE_ID);
+    }
+}
