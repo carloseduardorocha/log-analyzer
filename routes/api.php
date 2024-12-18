7727832577573cbee5fe5c1030e39d8a59a89b92
Carlos Rocha <carlos.rocha548@hotmail.com>
2024-12-18 17:26:56 -0300

    feat(api): created log routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\LogController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function() {
+
+    Route::prefix('logs')->name('logs.')->group(function() {
+        Route::controller(LogController::class)->group(function() {
+            Route::post('/process', 'process')->name('process');
+        });
+    });
 
     // Laravel Health Route
     Route::get('/ping', function() {
