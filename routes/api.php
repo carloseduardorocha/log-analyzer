d4f6d291412e2076b3a67e451b8829d499e5ff45
Carlos Rocha <carlos.rocha548@hotmail.com>
2024-12-18 22:40:28 -0300

    feat(api): created report routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\LogController;
+use App\Http\Controllers\ReportController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function() {
@@ -10,6 +12,15 @@ Route::prefix('v1')->group(function() {
             Route::post('/process', 'process')->name('process');
         });
     });
+
+    Route::prefix('reports')->name('reports.')->group(function() {
+        Route::controller(ReportController::class)->group(function() {
+            Route::get('/requests-by-consumer', 'processRequestsByConsumerReport')->name('requests-by-consumer');
+            Route::get('/requests-by-service', 'processRequestsByServiceReport')->name('requests-by-service');
+            Route::get('/average-times-by-service', 'processAverageTimesByServiceReport')->name('average-times-by-service');
+        });
+    });
+
 
     // Laravel Health Route
     Route::get('/ping', function() {
